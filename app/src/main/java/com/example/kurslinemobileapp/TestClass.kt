dd5a66d98343fa8065edda56eee8f743558055fc
alovekil <131664802+kursline2023@users.noreply.github.com>
2023-04-27 12:56:47 +0400

    testclass

--- app/src/main/java/com/example/kurslinemobileapp/TestClass.kt
+++ app/src/main/java/com/example/kurslinemobileapp/TestClass.kt
@@ -2,5 +2,4 @@ package com.example.kurslinemobileapp
 
 class TestClass {
     private  val emin : String = "Emin775"
-
 }
--- app/src/main/java/com/example/kurslinemobileapp/modelKerim.kt
+++ app/src/main/java/com/example/kurslinemobileapp/modelKerim.kt
@@ -1,7 +1,5 @@
 package com.example.kurslinemobileapp
 
  class modelKerim {
-
   private val kerim643: String = "Kerim645"
-
 }
