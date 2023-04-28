1f993f7459aaf123ae1b1d91fd34155011349df5
alovekil <109520227+alovekil@users.noreply.github.com>
2023-04-28 22:57:34 +0400

    commitKerim

--- app/src/main/java/com/example/kurslinemobileapp/MainActivity.kt
+++ app/src/main/java/com/example/kurslinemobileapp/MainActivity.kt
@@ -8,19 +8,23 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.kurslinemobileapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
      val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+         bottomNavigationView = binding.bottomNav
         setupWithNavController(bottomNavigationView,navController)
     }
 }
