2bad916a9a5799c180411910ca34a904f79078e5
alovekil <109520227+alovekil@users.noreply.github.com>
2023-04-28 18:23:31 +0400

    commitKerim

--- app/src/main/java/com/example/kurslinemobileapp/MainActivity.kt
+++ app/src/main/java/com/example/kurslinemobileapp/MainActivity.kt
@@ -2,17 +2,25 @@ package com.example.kurslinemobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        val navController = findNavController(R.id.fragmentContainerView)
-        bottomNavigation.setupWithNavController(navController)
+     val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        setupWithNavController(bottomNavigationView,navController)
     }
 }
--- app/src/main/java/com/example/kurslinemobileapp/UI/MainPageFragment.kt
+++ app/src/main/java/com/example/kurslinemobileapp/UI/MainPageFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.kurslinemobileapp.R
 
 
 class MainPageFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_main_page, container, false)
+
+        return view
 
     }
-
 
 }
