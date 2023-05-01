cd49a15cfd441a770587425a86d218ebb0ecc60b
alovekil <131664802+kursline2023@users.noreply.github.com>
2023-05-01 13:27:15 +0400

    Emin Commit

--- app/src/main/java/com/example/kurslinemobileapp/MainActivity.kt
+++ app/src/main/java/com/example/kurslinemobileapp/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.kurslinemobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.kurslinemobileapp.UI.FavoritePageFragment
+import com.example.kurslinemobileapp.UI.MainPageFragment
+import com.example.kurslinemobileapp.UI.RegisterFragment
 import com.example.kurslinemobileapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -22,9 +26,30 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val mainpageFragment=MainPageFragment()
+        val registrationpageFragment=RegisterFragment()
+        val favoritesFragment=FavoritePageFragment()
+
+        makeCurrentFragment(mainpageFragment)
+
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.mainPageFragment->makeCurrentFragment(mainpageFragment)
+                R.id.accountFragment->makeCurrentFragment(registrationpageFragment)
+                R.id.favorites->makeCurrentFragment(favoritesFragment)
+            }
+            true
+        }
+/*
      val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
          bottomNavigationView = binding.bottomNav
-        setupWithNavController(bottomNavigationView,navController)
+        setupWithNavController(bottomNavigationView,navController)*/
+    }
+
+
+    private fun makeCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+        replace(R.id.fragmentContainerView,fragment)
+        commit()
     }
 }
