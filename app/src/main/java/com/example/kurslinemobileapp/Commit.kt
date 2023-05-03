a6bb16020d0b34e09e6f433d9921762bfa411608
alovekil <131664802+kursline2023@users.noreply.github.com>
2023-05-03 13:10:52 +0400

    Emin Commit

---
+++ app/src/main/java/com/example/kurslinemobileapp/Commit.kt
@@ -0,0 +1,19 @@
+package com.example.kurslinemobileapp
+
+/*
+val mainpageFragment=MainPageFragment()
+val registrationpageFragment=RegisterFragment()
+val favoritesFragment=FavoritePageFragment()
+
+makeCurrentFragment(mainpageFragment)
+
+binding.bottomNav.setOnNavigationItemSelectedListener {
+    when(it.itemId){
+        R.id.mainPageFragment->makeCurrentFragment(mainpageFragment)
+        R.id.accountFragment->makeCurrentFragment(registrationpageFragment)
+        R.id.favorites->makeCurrentFragment(favoritesFragment)
+    }
+    true
+}
+
+ */
