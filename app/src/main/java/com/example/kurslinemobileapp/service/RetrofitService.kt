6443d3eca2debe9896a3492f66607e047fa5f296
alovekil <109520227+alovekil@users.noreply.github.com>
2023-05-03 13:24:52 +0400

    commitKerim

---
+++ app/src/main/java/com/example/kurslinemobileapp/service/RetrofitService.kt
@@ -0,0 +1,16 @@
+package com.example.kurslinemobileapp.service
+
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitService {
+    val client = OkHttpService().httpClient.build()
+    val url = "https://kursline.az/"
+    val retrofit = Retrofit.Builder()
+        .baseUrl(url)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .client(client)
+        .build()
+}
