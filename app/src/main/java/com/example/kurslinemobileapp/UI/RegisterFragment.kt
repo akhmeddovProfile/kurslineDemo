5727c284f684fb15c642efa6a61ac3dee379cc7e
alovekil <109520227+alovekil@users.noreply.github.com>
2023-05-03 14:22:35 +0400

    commitKerim

--- app/src/main/java/com/example/kurslinemobileapp/UI/RegisterFragment.kt
+++ app/src/main/java/com/example/kurslinemobileapp/UI/RegisterFragment.kt
@@ -26,6 +26,7 @@ import kotlinx.android.synthetic.main.fragment_register.view.*
 class RegisterFragment : Fragment() {
     var compositeDisposable = CompositeDisposable()
         var BASE_URL = "https://kursline.az/"
+
     @SuppressLint("SuspiciousIndentation")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,48 +35,26 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_register, container, false)
 
-        // Get the SharedPreferences object
-
-
         val registerButton = view.findViewById<Button>(R.id.registerButton)
         registerButton.setOnClickListener {
-
             val name = view.nameEditText.text.toString()
             val surname =view.surnameEditText.text.toString()
             val email = view.mailEditText.text.toString()
             val phone =view.phoneEditText.text.toString()
             val password = view.passwordEditText.text.toString()
             val password2 = view.confirmPasswordEditText.text.toString()
-
-
             // Validate input fields
             if (name.isEmpty() || surname.isEmpty() || email.isEmpty() || password.isEmpty() || password2.isEmpty() || phone.isEmpty()) {
                 Toast.makeText(requireContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show()
-            } else {
+            } else if(password != password2){
+                Toast.makeText(requireContext(),"Passwords is not equal", Toast.LENGTH_SHORT).show()
+            }else {
                 // Save user registration data to shared preferences
-                val request = RegisterModel(
-                    name,
-                    email,
-                    phone,
-                    password,
-                    1
-                )
                 val sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
                 val editor = sharedPreferences.edit()
-
-                /*
-                editor.putString("name", name)
-                editor.putString("surname", surname)
-                editor.putString("email", email)
-                editor.putString("phone", phone)
-                editor.putString("password", password)
-
-                 */
                 editor.putBoolean("is_registered", true)
                 editor.apply()
-
                 register(name,email,phone,password,1)
-
             }
         }
 
@@ -100,4 +79,5 @@ class RegisterFragment : Fragment() {
         println("Response: " + response)
         findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
     }
+
 }
