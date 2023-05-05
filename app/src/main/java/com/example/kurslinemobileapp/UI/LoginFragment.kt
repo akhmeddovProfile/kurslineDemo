5c5535396d466659bcec9429bc29c1ae0662ed98
alovekil <109520227+alovekil@users.noreply.github.com>
2023-05-05 12:07:25 +0400

    commitKerim

--- app/src/main/java/com/example/kurslinemobileapp/UI/LoginFragment.kt
+++ app/src/main/java/com/example/kurslinemobileapp/UI/LoginFragment.kt
@@ -1,9 +1,14 @@
 package com.example.kurslinemobileapp.UI
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -30,12 +35,16 @@ class LoginFragment : Fragment() {
 
     private var compositeDisposableLogin: CompositeDisposable? = null
     private lateinit var sharedPreferences: SharedPreferences
+    @SuppressLint("ResourceAsColor")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+
         val loginButton = view.findViewById<Button>(R.id.loginButton)
         loginButton.setOnClickListener {
             val email = emailLoginEditText.text.toString()
