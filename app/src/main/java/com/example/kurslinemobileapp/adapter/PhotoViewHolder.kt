d43e2b0bcaeb7c8dce1864f0cea6fac3619089cc
alovekil <109520227+alovekil@users.noreply.github.com>
2023-05-27 21:38:11 +0400

    commitKerim

---
+++ app/src/main/java/com/example/kurslinemobileapp/adapter/PhotoViewHolder.kt
@@ -0,0 +1,20 @@
+package com.example.kurslinemobileapp.adapter
+
+import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kurslinemobileapp.R
+import com.example.kurslinemobileapp.model.Photo
+import com.squareup.picasso.Picasso
+
+class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(photo: Photo) {
+        // Use Glide or Picasso to load and display the photo into an ImageView
+        val imageView: ImageView = itemView.findViewById(R.id.imageViewPager)
+
+        // Using Picasso:
+        Picasso.get()
+            .load(photo.uri)
+            .into(imageView)
+    }
+}
