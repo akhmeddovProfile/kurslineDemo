97755a8b8b897e559c57f8af03081889073faba1
alovekil <109520227+alovekil@users.noreply.github.com>
2023-05-28 21:15:05 +0400

    commitKerim

---
+++ app/src/main/java/com/example/kurslinemobileapp/view/courseFmAc/CourseBusinessProfile.kt
@@ -0,0 +1,11 @@
+package com.example.kurslinemobileapp.view.courseFmAc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourseBusinessProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_business_profile)
+    }
+}
