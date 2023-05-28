0a007cb249de54f811d8f8706c11f164c532d84a
alovekil <131664802+kursline2023@users.noreply.github.com>
2023-05-28 23:22:39 +0400

    commit
    emincommit

--- app/src/main/java/com/example/kurslinemobileapp/view/loginRegister/RegisterCompanyActivity.kt
+++ app/src/main/java/com/example/kurslinemobileapp/view/loginRegister/RegisterCompanyActivity.kt
@@ -1,11 +1,28 @@
 package com.example.kurslinemobileapp.view.loginRegister
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.kurslinemobileapp.R
 import kotlinx.android.synthetic.main.activity_register_company.*
+import java.io.IOException
 
 class RegisterCompanyActivity : AppCompatActivity() {
+    //Image
+    var selectedPicture : Uri? = null
+    var selectedBitmap : Bitmap? = null
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_company)
@@ -27,7 +44,48 @@ class RegisterCompanyActivity : AppCompatActivity() {
     }
 
     private fun downloadPhotoFromGalery() {
-
+        activityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intentFromResult = result.data
+                if (intentFromResult != null) {
+                    selectedPicture = intentFromResult.data
+                    try {
+                        if (Build.VERSION.SDK_INT >= 28) {
+                            val source = ImageDecoder.createSource(
+                                requireContext().contentResolver,
+                                selectedPicture!!
+                            )
+                            selectedBitmap = ImageDecoder.decodeBitmap(source)
+                            requireView().certificateImage.setImageBitmap(selectedBitmap)
+                        } else {
+                            selectedBitmap = MediaStore.Images.Media.getBitmap(
+                                requireContext().contentResolver,
+                                selected
+                            )
+                            requireView().certificateImage.setImageBitmap(selectedBitmap)
+                        }
+                        val cervicatePath = selectedPicture?.path
+                        lawyerCertificate.setText(cervicatePath!!.substring((cervicatePath.length - 8) , cervicatePath.length)+".JPG")
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+        permissionLauncherCertificate = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { result ->
+            if (result) {
+                //permission granted
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncherCertificate.launch(intentToGallery)
+            } else {
+                //permission denied
+                Toast.makeText(requireContext(), "Permisson needed!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
