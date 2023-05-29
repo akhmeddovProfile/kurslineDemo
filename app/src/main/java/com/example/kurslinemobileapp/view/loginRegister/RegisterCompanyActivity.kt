a3932fa41abaefaefcdc07ed2f4decb42d07116b
alovekil <131664802+kursline2023@users.noreply.github.com>
2023-05-29 22:11:18 +0400

    Emin Commit

--- app/src/main/java/com/example/kurslinemobileapp/view/loginRegister/RegisterCompanyActivity.kt
+++ app/src/main/java/com/example/kurslinemobileapp/view/loginRegister/RegisterCompanyActivity.kt
@@ -2,18 +2,22 @@ package com.example.kurslinemobileapp.view.loginRegister
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.kurslinemobileapp.R
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_register_company.*
 import java.io.IOException
 
@@ -40,9 +44,30 @@ class RegisterCompanyActivity : AppCompatActivity() {
             val companyCategoryContainer = companyCategoryEditText.text.toString()
             val aboutCompanyContainer = aboutCompanyEditText.text.toString()
         }
+        downloadPhotoFromGalery()
 
+        companyPhoto.setOnClickListener {
+            selectCertificate(it)
+        }
     }
 
+
+    private fun selectCertificate(view: View){
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission",
+                    View.OnClickListener {
+                        permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }).show()
+            } else {
+                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        } else {
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            activityResultLauncher.launch(intentToGallery)
+        }
+    }
     private fun downloadPhotoFromGalery() {
         activityResultLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
@@ -54,36 +79,37 @@ class RegisterCompanyActivity : AppCompatActivity() {
                     try {
                         if (Build.VERSION.SDK_INT >= 28) {
                             val source = ImageDecoder.createSource(
-                                requireContext().contentResolver,
+                                this.contentResolver,
                                 selectedPicture!!
                             )
                             selectedBitmap = ImageDecoder.decodeBitmap(source)
-                            requireView().certificateImage.setImageBitmap(selectedBitmap)
+
+                            certificateImage.setImageBitmap(selectedBitmap)
                         } else {
                             selectedBitmap = MediaStore.Images.Media.getBitmap(
-                                requireContext().contentResolver,
-                                selected
+                                this.contentResolver,
+                                selectedPicture
                             )
-                            requireView().certificateImage.setImageBitmap(selectedBitmap)
+                            certificateImage.setImageBitmap(selectedBitmap)
                         }
                         val cervicatePath = selectedPicture?.path
-                        lawyerCertificate.setText(cervicatePath!!.substring((cervicatePath.length - 8) , cervicatePath.length)+".JPG")
+                        companyPhoto.setText(cervicatePath!!.substring((cervicatePath.length - 8) , cervicatePath.length)+".JPG")
                     } catch (e: IOException) {
                         e.printStackTrace()
                     }
                 }
             }
         }
-        permissionLauncherCertificate = registerForActivityResult(
+        permissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestPermission()
         ) { result ->
             if (result) {
                 //permission granted
                 val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                activityResultLauncherCertificate.launch(intentToGallery)
+                activityResultLauncher.launch(intentToGallery)
             } else {
                 //permission denied
-                Toast.makeText(requireContext(), "Permisson needed!", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@RegisterCompanyActivity, "Permisson needed!", Toast.LENGTH_LONG).show()
             }
         }
     }
