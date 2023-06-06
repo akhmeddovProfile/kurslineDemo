e2df10f633b0d5373494ae796c215b9696d894b3
alovekil <109520227+alovekil@users.noreply.github.com>
2023-06-06 10:53:54 +0400

    commitKerim

--- app/src/main/java/com/example/kurslinemobileapp/view/accountsFragments/BusinessTransactionProfileFragment.kt
+++ app/src/main/java/com/example/kurslinemobileapp/view/accountsFragments/BusinessTransactionProfileFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.kurslinemobileapp.R
+import com.example.kurslinemobileapp.view.fragments.FilterFragment
+import kotlinx.android.synthetic.main.fragment_business_transactions_profile.view.*
 
 class BusinessTransactionProfileFragment : Fragment() {
 
@@ -17,6 +20,20 @@ class BusinessTransactionProfileFragment : Fragment() {
         val view =
             inflater.inflate(R.layout.fragment_business_transactions_profile, container, false)
 
+        val goButton = view.findViewById<ImageView>(R.id.goToBusinessAccountFragment)
+        goButton.setOnClickListener {
+            println("1")
+            val fragmentManager = requireFragmentManager()
+            // Start a fragment transaction
+            val transaction = fragmentManager.beginTransaction()
+            // Replace the first fragment with the second fragment
+            transaction.replace(R.id.transactionProfileRl, BusinessAccountFragment())
+            transaction.setReorderingAllowed(true)
+            // Add the transaction to the back stack
+            transaction.addToBackStack(null)
+            // Commit the transaction
+            transaction.commit()
+        }
         return view
     }
 
