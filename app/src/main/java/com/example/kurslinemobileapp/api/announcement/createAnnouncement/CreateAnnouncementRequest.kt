a1c4a78f3648c8f8f62afc6643d0606214de768b
alovekil <131664802+kursline2023@users.noreply.github.com>
2023-06-20 19:10:16 +0400

    Emin Commit

--- app/src/main/java/com/example/kurslinemobileapp/api/announcement/createAnnouncement/CreateAnnouncementRequest.kt
+++ app/src/main/java/com/example/kurslinemobileapp/api/announcement/createAnnouncement/CreateAnnouncementRequest.kt
@@ -4,22 +4,13 @@ package com.example.kurslinemobileapp.api.announcement.createAnnouncement
 import com.google.gson.annotations.SerializedName
 
 data class CreateAnnouncementRequest(
-    @SerializedName("announcementAddress")
     val announcementAddress: String,
-    @SerializedName("announcementDesc")
     val announcementDesc: String,
-    @SerializedName("announcementIsOnlineId")
     val announcementIsOnlineId: Int,
-    @SerializedName("announcementName")
     val announcementName: String,
-    @SerializedName("announcementPrice")
     val announcementPrice: Int,
-    @SerializedName("announcementRegionId")
     val announcementRegionId: Int,
-    @SerializedName("announcementSubCategoryId")
     val announcementSubCategoryId: Int,
-    @SerializedName("img")
     val img: List<Img>,
-    @SerializedName("teacher")
     val teacher: List<String>
 )
