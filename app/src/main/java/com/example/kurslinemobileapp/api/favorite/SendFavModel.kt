72ce7457bed717c9999015e52fd06487481f9983
alovekil <131664802+kursline2023@users.noreply.github.com>
2023-06-26 22:08:06 +0400

    Emin Commit

--- app/src/main/java/com/example/kurslinemobileapp/api/favorite/SendFavModel.kt
+++ app/src/main/java/com/example/kurslinemobileapp/api/favorite/SendFavModel.kt
@@ -1,7 +1,11 @@
 package com.example.kurslinemobileapp.api.favorite
 
 data class SendFavModel(
+    var userid:Int,
     var productid : Int,
-    var userId:Int,
-    var token:String
+/*    var userId:Int,
+    var token:String,*/
+
+    var isSelected:Boolean=false
+
 )
