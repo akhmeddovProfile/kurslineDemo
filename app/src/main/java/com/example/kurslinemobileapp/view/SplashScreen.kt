c64999ca7ec2cb3e83e5cba5333916cdfa5032ff
alovekil <109520227+alovekil@users.noreply.github.com>
2023-07-04 17:09:46 +0400

    commitKerim

---
+++ app/src/main/java/com/example/kurslinemobileapp/view/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.example.kurslinemobileapp.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.kurslinemobileapp.R
+
+class SplashScreen : AppCompatActivity() {
+    private val splashDuration = 3000L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish() // Close the splash screen activity
+        }, splashDuration)
+    }
+}
