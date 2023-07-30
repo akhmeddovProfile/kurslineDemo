3b085407cfe0dbbd66e935b4a2df8dbd59d8e71d
alovekil <131664802+kursline2023@users.noreply.github.com>
2023-07-30 14:43:22 +0400

    Emin Commit

---
+++ app/src/main/java/com/example/kurslinemobileapp/view/payment/EnterCardNumberPage.kt
@@ -0,0 +1,12 @@
+package com.example.kurslinemobileapp.view.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kurslinemobileapp.R
+
+class EnterCardNumberPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_card_number_page)
+    }
+}
---
+++ app/src/main/java/com/example/kurslinemobileapp/view/payment/MoveForwardAnn.kt
@@ -0,0 +1,13 @@
+package com.example.kurslinemobileapp.view.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kurslinemobileapp.R
+
+class MoveForwardAnn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_forward_ann)
+
+    }
+}
